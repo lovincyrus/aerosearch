4db827da7492d8c3765723e4b8e4f3d8630414e2
Can Yildirim <canpy30@gmail.com>
2015-11-07 14:07:53 -0800

    working on image search

---
+++ routes/image-search.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+/*
+function getKeyword(url){
+	var keyword = "-99";
+	
+	
+	
+	return keyword;
+}
+*/
+/* GET home page. */
+router.get('/aerosearch', function(req, res, next) {
+	var imageURL = req.body.url;
+	//var keyword = getKeyword(imageURL);
+	
+	
+	
+  res.render('index', {
+  	title: 'Sign In to AeroFS'
+  });
+});
+
+module.exports = router;
